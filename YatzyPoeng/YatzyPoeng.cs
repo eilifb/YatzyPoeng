9505c9e5332e64bf9db1e4fb45ddb63792a81c4b
Eilif B <eilifb@gmail.com>
2022-03-21 15:44:27 +0100

    La til funksjonalitet for å beregne poeng for et og to par, samt la til noen enhetstester for de beregningene. Skrev om på måten enere, toere og de kategoriene blir beregnet.

--- YatzyPoeng/YatzyPoeng.cs
+++ YatzyPoeng/YatzyPoeng.cs
@@ -10,36 +10,63 @@ namespace YatzyPoengNS
         {
             List<int> terninger = terningerInput.Split(",").Select(t => { return System.Convert.ToInt32(t); }).ToList();
 
-            if (terninger.Where(t => t >= 6 || t < 1).Any() || terninger.Count() != 5)
+            if (terninger.Where(t => t > 6 || t < 1).Any() || terninger.Count() != 5)
                 throw new ArgumentException("Terningene må være gyldige! (5 heltall mellom 1 og 6!)",terningerInput);
 
+            Dictionary<int, List<int>> terningerSamlet = new Dictionary<int, List<int>>();
+            for (int i = 1; i <= 6; i++)
+                terningerSamlet.Add(i, terninger.Where(t => t == i).ToList());
+
+
             int poeng = 0;
-            int? sorter = null;
+            List<int> terningerSum;
+
 
             switch (kategori)
             {
                 case "Enere":
-                    sorter = 1;
+                    poeng = terningerSamlet[1].Sum();
                     break;
                 case "Toere":
-                    sorter = 2;
+                    poeng = terningerSamlet[2].Sum();
                     break;
                 case "Treere":
-                    sorter = 3;
+                    poeng = terningerSamlet[3].Sum();
                     break;
                 case "Firere":
-                    sorter = 4;
+                    poeng = terningerSamlet[4].Sum();
                     break;
                 case "Femere":
-                    sorter = 5;
+                    poeng = terningerSamlet[5].Sum();
                     break;
                 case "Seksere":
-                    sorter = 6;
+                    poeng = terningerSamlet[6].Sum();
+                    break;
+
+                case "Par":
+                    terningerSum = terningerSamlet
+                                    .Where(t => t.Value.Count > 1)
+                                    .Select(t => t.Value.First()*2) //Grunnen til at jeg har Value.First()*2 isteden for Value.Sum() er i tilfelle hvor det er mer en to terninger som har samme øye.
+                                    .ToList();
+
+                    poeng = terningerSum.Count() != 0 ? terningerSum.Max() : 0;
+                    break;
+
+                case "ToPar":
+                    if (terningerSamlet.Where(t => t.Value.Count > 4).Any()) //Spesialtilfelle: de to parene har samme øye.
+                        //Det fungerer fint å bare ta det første øyet i terningerSamlet som har 2 par, da det bare er et øye som kan ha to par, gitt 5 terninger. 
+                        poeng = terningerSamlet.Where(t => t.Value.Count > 4).First().Value.First() * 4; 
+                    else
+                    {
+                        terningerSum = terningerSamlet
+                                    .Where(t => t.Value.Count > 1)
+                                    .Select(t => t.Value.First() * 2)
+                                    .ToList();
+
+                        poeng = terningerSum.Count() != 0 ? terningerSum.Count() != 2 ? 0 : terningerSum[^1] + terningerSum[^2] : 0;
+                    }
                     break;
             }
-
-            if (sorter != null)
-                poeng = terninger.Where(t => t == sorter).Count() * sorter.Value;
 
             return poeng;
         }
--- YatzyTests/YatzyTests.cs
+++ YatzyTests/YatzyTests.cs
@@ -17,8 +17,25 @@ namespace YatzyTestsNS
         [TestMethod]
         public void TestSorter()
         {
-            Assert.AreEqual(yp.BeregnPoeng("1,1,1,1,1", "Enere"), 5);
-            Assert.AreEqual(yp.BeregnPoeng("1,2,4,5,5", "Femere"), 10);
+            Assert.AreEqual(5, yp.BeregnPoeng("1,1,1,1,1", "Enere"));
+            Assert.AreEqual(10, yp.BeregnPoeng("1,2,4,5,5", "Femere"));
+        }
+
+        [TestMethod]
+        public void TestPar()
+        {
+            Assert.AreEqual(0, yp.BeregnPoeng("1,2,3,4,5", "Par"));
+            Assert.AreEqual(4, yp.BeregnPoeng("2,1,2,3,5", "Par"));
+            Assert.AreEqual(10, yp.BeregnPoeng("1,1,2,5,5", "Par"));
+            Assert.AreEqual(4, yp.BeregnPoeng("2,2,2,2,2", "Par"));
+        }
+
+        [TestMethod]
+        public void TestToPar()
+        {
+            Assert.AreEqual(0, yp.BeregnPoeng("1,3,5,6,1", "ToPar"));
+            Assert.AreEqual(10, yp.BeregnPoeng("3,1,4,1,4", "ToPar"));
+            Assert.AreEqual(20, yp.BeregnPoeng("5,5,5,5,5", "ToPar"));
         }
 
         [TestMethod]
